06fda60cab58070866bf91589c0af2c4864bbdd6
lcgkiller <lcgkiller@gmail.com>
2017-07-10 14:46:07 +0900

    클래스 기반뷰(API View)로 전환

--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-
-from . import views
+from snippets import views
 
 urlpatterns = [
-    url(r'^$', views.snippet_list),
-    url(r'^(?P<pk>\d+)/$', views.snippet_detail)
+        # url(r'^$', views.snippet_list),
+        # url(r'^(?P<pk>\d+)/$', views.snippet_detail)
+
+    # 클래스 기반 뷰
+    url(r'^$', views.SnippetList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
